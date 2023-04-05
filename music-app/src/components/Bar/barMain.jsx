51bfc7b99af042d531fef4519a9097d0160940ae
Georgy Komarov <komarovgosha001@gmail.com>
2023-04-05 22:45:09 +0300

    wow

--- music-app/src/components/Bar/barMain.jsx
+++ music-app/src/components/Bar/barMain.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 import BarLeft from './barLeft';
 
@@ -11,19 +11,31 @@ import audio  from '../../asd.mp3';
 function Bar(){
     const audioRef = useRef(null);
     const progressBarRef = useRef(null);
+    const [isPlaying, setPlaying] = useState(false)
 
-    const handleProgressChange = () => {
-        audioRef.current.currentTime = progressBarRef.current.value;
-    };
+    // const handleProgressChange = () => {
+    //     audioRef.current.currentTime = progressBarRef.current.value;
+        
+    // };
+
+    useEffect(() => {
+    if (audioRef.current) {
+        audioRef.current.ontimeupdate = () => {
+        progressBarRef.current.value =
+            (audioRef.current.currentTime / audioRef.current.duration) * 1000
+        }
+    }
+    }, [isPlaying])
 
     return (
      <S.Bar>
          <audio controls src={audio} ref={audioRef}  style={{display: "none"}}/>
         <S.BarContent>
-            <S.BarPlayerProgress type="range"
+            <S.BarPlayerProgress 
+                type="range"
                 ref={progressBarRef}
                 defaultValue="0"
-                onChange={handleProgressChange}/>
+                onChange={setPlaying}/>
             <S.BarlayerBlock>
                 <BarLeft audioRef={audioRef} />
                 <S.BarVolumeBlock>
